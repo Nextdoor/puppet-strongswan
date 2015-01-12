07c5be44574722f4a838f43d0f649fbd331650c3
Matt Wise <matt@nextdoor.com>
2015-01-12 15:17:21 -0800

    Fix the meraki unit tests

--- spec/defines/presets_meraki_vpn_spec.rb
+++ spec/defines/presets_meraki_vpn_spec.rb
@@ -5,8 +5,8 @@ describe 'strongswan::presets::meraki_vpn', :type => 'define' do
 
   context 'main test' do
     let(:params) {{
-      :meraki_public_ip => 'MERAKI_PUBLIC_IP',
-      :meraki_subnet    => 'MERAKI_SUBNET',
+      :meraki_public_ip => '123.123.123.123',
+      :meraki_subnet    => '192.168.0.0/24',
       :swan_public_ip   => 'SWAN_PUBLIC_IP',
       :swan_subnet      => 'SWAN_SUBNET',
       :psk              => 'PSK',
@@ -18,15 +18,15 @@ describe 'strongswan::presets::meraki_vpn', :type => 'define' do
       should contain_class('strongswan::env')
 
       should contain_firewall('020 UNITTEST inbound udp').with(
-        'source' => 'MERAKI_SUBNET')
+        'source' => '123.123.123.123')
       should contain_firewall('020 UNITTEST masquerading').with(
         'ensure' => 'present',
-        'source' => 'MERAKI_SUBNET')
-      should contain_firewall('020 UNITTEST ipsec routing policy').with(
-        'source' => 'MERAKI_SUBNET')
+        'source' => '192.168.0.0/24')
+      should contain_firewall('021 UNITTEST ipsec routing policy').with(
+        'source' => '192.168.0.0/24')
 
       should contain_strongswan__conn('UNITTEST').with(
-        'params'  => /MERAKI_PUBLIC_IP/,
+        'params'  => /123.123.123.123/,
         'secrets' => /PSK/)
     end
   end
