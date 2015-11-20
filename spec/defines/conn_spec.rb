ed0685d1987b1779675fbb5c38c0e6e250dd726e
Justin Bronn <justin@counsyl.com>
2015-11-20 15:15:19 -0600

    Add test for RSA/ECDSA private key files in secrets.

--- spec/defines/conn_spec.rb
+++ spec/defines/conn_spec.rb
@@ -29,4 +29,28 @@ describe 'strongswan::conn', :type => 'define' do
         'content' => /foo2=bar2/)
     end
   end
+
+  context 'sample-pubkey-connection' do
+    let(:params) {{
+      :params => {
+        'left'      => '1.2.3.4',
+        'leftcert'  => 'foo.example.dev',
+        'leftid'    => '@foo.example.dev.pem'
+        'right'     => '2.3.4.5',
+        'rightid'   => '@bar.example.dev',
+      },
+      :secrets => [
+        { 'auth' => 'RSA', 'key' => 'foo.example.dev.pem' },
+        { 'auth' => 'ECDSA', 'key' => 'bar.example.dev.pem',
+          'passphrase' => 'foobar'}
+      ],
+    }}
+    it do
+      should compile.with_all_deps
+      should contain_file('/etc/ipsec.d/secrets/ipsec.unittest.secrets').with(
+        'content' => /: RSA foo.example.dev.pem/)
+      should contain_file('/etc/ipsec.d/secrets/ipsec.unittest.secrets').with(
+        'content' => /: ECDSA bar.example.dev.pem "foobar"/)
+    end
+  end
 end
